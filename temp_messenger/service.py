65f5673c281a0b065e288b1a7dcbf651d45b0089
owezzy <owenobezzy@gmail.com>
2019-04-25 15:28:13 +0300

    add http protocol to service

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -1,11 +1,20 @@
-from nameko.rpc import rpc
+from nameko.rpc import rpc, RpcProxy
+from nameko.web.handlers import http
 
 
 class KonnichiwaService:
 
-    name = 'konnnichiwa_service'
+    name = 'konnichiwa_service'
 
     @rpc
     def konnichiwa(self):
         return 'Konnichiwa!'
 
+
+class WebServer:
+    name = 'web_server'
+    konnichiwa_service = RpcProxy('konnichiwa_service')
+
+    @http('GET', '/')
+    def home(self, request):
+        return self.konnichiwa_service.konnichiwa()
