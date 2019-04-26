3d8827094b3c6a90578afd00911edb7041bd9035
owezzy <owenobezzy@gmail.com>
2019-04-26 13:16:21 +0300

    add save_message method to message service

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -10,3 +10,8 @@ class MessageService:
     @rpc
     def get_message(self, message_id):
         return self.message_store.get_message(message_id)
+
+    @rpc
+    def save_message(self, message):
+        message_id = self.message_store.save_message(message)
+        return message_id
