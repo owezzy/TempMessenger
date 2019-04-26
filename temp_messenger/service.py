47b6d8e54d571758d556ad4cc0c6038a774bed6f
owezzy <owenobezzy@gmail.com>
2019-04-26 17:39:32 +0300

    create web server for CRUD operation on message_service

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -1,9 +1,17 @@
-from nameko.rpc import rpc
+from nameko.rpc import rpc, RpcProxy
+from nameko.web.handlers import http
+from werkzeug.wrappers import Response
+
 from .dependencies.redis import MessageStore
+from .dependencies.jinja2 import Jinja2
+
+
+def create_html_response(content):
+    headers = {'Content-Type': 'text/html'}
+    return Response(content, status=200, headers=headers)
 
 
 class MessageService:
-
     name = 'message_service'
     message_store = MessageStore()
 
@@ -20,3 +28,18 @@ class MessageService:
     def get_all_messages(self):
         messages = self.message_store.get_all_messages()
         return messages
+
+
+class WebServer:
+
+    name = 'web_server'
+    message_service = RpcProxy('message_service')
+    jinja = Jinja2
+
+    @http('GET', '/')
+    def home(self, request):
+        messages = self.message_service.get_all_messages()
+        rendered_template = self.jinja.render_home(messages)
+        html_response = create_html_response(rendered_template)
+
+        return html_response
