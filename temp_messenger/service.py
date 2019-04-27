81a10b553e8663d1f87693cd61f597b23bbe2701
owezzy <owenobezzy@gmail.com>
2019-04-27 14:47:09 +0300

    add POST messages endpoint to webserver microservice

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -1,6 +1,7 @@
 from nameko.rpc import rpc, RpcProxy
 from nameko.web.handlers import http
 from werkzeug.wrappers import Response
+import json
 
 from .dependencies.redis import MessageStore
 from .dependencies.jinja2 import Jinja2
@@ -41,5 +42,21 @@ class WebServer:
         messages = self.message_service.get_all_messages()
         rendered_template = self.jinja.render_home(messages)
         html_response = create_html_response(rendered_template)
-
         return html_response
+
+    @http('POST', '/messages')
+    def post_message(self, request):
+        data_as_text = request.get_data(as_text=True)
+
+        try:
+            data = json.loads(data_as_text)
+        except json.JSONDecodeError:
+            return 400, 'JSON payload expected'
+
+        try:
+            message = data['message']
+        except KeyError:
+            return 400, 'No message given'
+
+        self.message_service.save_message(message)
+        return 204, ''
